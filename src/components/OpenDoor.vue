118936c58d1575ebee387237b6ed3b973c995c64
Dolapo Toki <dolapo@dtoki.io>
2023-12-18 11:43:11 -0800

    testing open door

---
+++ src/components/OpenDoor.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <button @click="openDoor">Open Door</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'App',
+    methods: {
+        async openDoor() {
+            try {
+                const response = await axios.post('http://localhost:8081/open-door');
+                alert(response.data.message);
+            } catch (error) {
+                alert(error.response ? error.response.data.error : 'Failed to reach the server');
+            }
+        }
+    }
+};
+</script>
+
+<style>
+/* Your styles here */
+</style>
